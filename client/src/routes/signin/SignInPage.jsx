f66dd19bdfd181589670acbdfd8c4f9c57574a26
dp189 <debabratapal4a@gmail.com>
2024-09-30 12:29:08 +0530

    Redirect issue solved

--- client/src/routes/signin/SignInPage.jsx
+++ client/src/routes/signin/SignInPage.jsx
@@ -4,10 +4,12 @@ import React from 'react'
 
 import './signIn.css';
 
+const forcedUrl = import.meta.env.PROD_DOMAIN || 'http://localhost:5173';
+
 const SignInPage = () => {
   return (
     <div className="signPage" id="signIn" >
-      <SignIn path="/sign-in" signUpUrl="/sign-up" forceRedirectUrl="/dashboard"/>
+      <SignIn path="/sign-in" signUpUrl="/sign-up" forceRedirectUrl={`${forcedUrl}/dashboard`}/>
     </div>
   )
 }
